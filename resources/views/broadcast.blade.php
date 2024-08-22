03d5e35712113427b58f246f2d00ab905b4c01b4
Adan <elahiadan7@gmail.com>
2024-08-22 16:13:22 +0530

    basic scarf holding is completed

---
+++ resources/views/broadcast.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Broadcasting</title>
+    <style>
+        {!! Vite::content('resources/css/app.css') !!}
+    </style>
+    <script>
+        {!! Vite::content('resources/js/app.js') !!}
+    </script>
+</head>
+
+<body>
+    <h1>Broadcasting message</h1>
+    <script>
+        Echo.channel('channel-name')
+            .listen('BroadcasrEvent', (e) => {
+                console.log(e);
+            });
+    </script>
+
+</body>
+
+</html>
